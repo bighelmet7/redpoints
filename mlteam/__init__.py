d589b22e82cbb6afb9895c75f7cfe1cdbcc39649
Abner Andino <brain.agam@gmail.com>
2019-08-31 21:24:26 +0200

    TASK: flask_redis package added as a extension

--- mlteam/__init__.py
+++ mlteam/__init__.py
@@ -1,5 +1,4 @@
-from flask import Flask, g
-from redis import Redis
+from flask import Flask
 
 def create_app(config_obj='mlteam.config.DevelopmentConfig'):
     """
@@ -7,6 +6,12 @@ def create_app(config_obj='mlteam.config.DevelopmentConfig'):
     """
     app = Flask(__name__)
     app.config.from_object(config_obj)
+
+    from v1.blueprint import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    from mlteam.extensions import redis_client
+    redis_client.init_app(app)
 
     from flask_cors import CORS
     CORS(app)
