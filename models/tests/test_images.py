efcd2aa5b67ccfd2ced66540eaa4960cf75b34bf
Abner Andino <brain.agam@gmail.com>
2019-09-01 09:04:44 +0200

    TASK: BatchImage testing model

--- models/tests/test_images.py
+++ models/tests/test_images.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from collections import namedtuple
 from unittest.mock import patch
 from unittest import TestCase
 
@@ -7,10 +8,13 @@ import requests_mock
 from PIL import Image, ImageFile
 from PIL.GifImagePlugin import GifImageFile
 from requests.exceptions import ReadTimeout
+from redis import Redis
+from simplejson import loads
 
+from const.redis_queue import BATCH_PREDICT
 from exceptions import ImageInfoError
 from mlteam.extensions import session
-from models.images import ImageInfo
+from models.images import ImageInfo, BatchImage
 
 # ISSUE: https://github.com/python-pillow/Pillow/issues/1510
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -132,3 +136,63 @@ class ImageInfoTest(TestCase):
 
     def tearDown(self):
         self.blank_image_64_64.close()
+
+
+class BatchImageTest(TestCase):
+
+    def setUp(self):
+        ImageInfoTSV = namedtuple('ImageInfoTSV', ['id', 'url'])
+        self.images = [
+            ImageInfoTSV(id=0, url="https://www.url.com/blank_image"),
+        ]
+        self.session = session
+        # Redis testing propouse.
+        self.redis_client = Redis(host='localhost', port=6379, db=0)
+
+    def test_send_to_redis_queue_valid_connection(self):
+        batch_images = BatchImage(
+            batch_size=2,
+        )
+        blank_image_64_64 = Image.new('RGB', (64,64))
+        images = [
+            np.array(blank_image_64_64).tolist()
+        ]
+        n_channels = 3
+        queue = 'queue:tst-batch-predict'
+        batch_images._send_to_redis_queue(n_channels, 64, 64, images, self.redis_client, queue)
+        result_str = self.redis_client.rpop(queue)
+        result = loads(result_str)
+        expected_batch_dimension = '({batch_size}, {ch}, {x}, {y})'.format(
+            batch_size=batch_images.batch_size,
+            ch=n_channels,
+            x=64,
+            y=64
+        )
+        expected = {
+            'batch_dimension': expected_batch_dimension,
+            'images': list(images)
+        }
+        self.assertEqual(result, expected)
+
+    def test_resize_batch_images(self):
+        batch_images = BatchImage(
+            images=self.images,
+            batch_size=2,
+        )
+        queue = 'queue:tst-batch-predict'
+        blank = Image.new('RGB', (80, 80))
+        # This is necessary for giving a format to our blank image.
+        with BytesIO () as output:
+            blank.save(output, format="GIF")
+            img_buf = output.getvalue()
+        with requests_mock.mock() as m:
+            m.get('https://www.url.com/blank_image', content=img_buf)
+            batch_images.resize_batch_images(redis_conn=self.redis_client, queue=queue)
+            result_str = self.redis_client.rpop(queue)
+            result = loads(result_str)
+            # The new sizes of the previous image 80x80.
+            expected = (64,64,)
+            self.assertEqual(np.shape(result['images'][0]), expected)
+
+    def tearDown(self):
+        self.redis_client.flushdb()
